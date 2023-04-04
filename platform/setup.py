4e32b50aadb5c9efd113c9ed80becd4ef7fdb502
Rodriguez <xavier.rodriguez@advans-group.com>
2023-04-04 11:16:34 +0200

    try to change to way dependcies are declared

--- platform/setup.py
+++ platform/setup.py
@@ -24,14 +24,14 @@ setup(
     cmdclass={'install': CustomInstallCommand},
 
     install_requires=[
-        'aardvark-py>=5.40',
-        'colorama>=0.4.6',
-        'paho-mqtt>=1.6.1',
-        'pyftdi>=0.54.0',
-        'pymodbus>=3.2.0',
-        'pyserial>=3.5',
-        'pyudev>=0.24.0',
-        'pyusb>=1.2.1',
+        'aardvark-py',
+        'colorama',
+        'paho-mqtt',
+        'pyftdi',
+        'pymodbus',
+        'pyserial',
+        'pyudev',
+        'pyusb',
     ],
 
     classifiers=[
