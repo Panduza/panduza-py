da13b3abeec187e6fdc5bca4a3a34e53d9afa678
Paul FISHER <fisherhpaul@gmail.com>
2023-04-26 09:28:32 +0200

    add keyword to robot framework

--- client/panduza/robof/dio.py
+++ client/panduza/robof/dio.py
@@ -1,7 +1,5 @@
 from robotlibcore import keyword
 from robot.libraries.BuiltIn import BuiltIn
-from hamcrest import assert_that, equal_to, any_of
-from .utils import boolean_str_to_boolean
 
 class KeywordsDio(object):
 
@@ -16,18 +14,21 @@ class KeywordsDio(object):
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].direction.value.set(float(direction), ensure)
-
+    
+    @keyword
     def set_direction_pull(self, dio_alias, pull, ensure=True):
         """Set the power supply voltage goal
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].direction.pull.set(float(pull), ensure)
+
     @keyword
     def set_direction_polling_cycle(self, dio_alias:str, cycle:float, ensure=True):
         """Set the power supply voltage goal
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].direction.polling_cycle.set(float(cycle), ensure)
+
 
     ###########################################################################
     # State
@@ -40,12 +41,13 @@ class KeywordsDio(object):
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].state.active.set(bool(active), ensure)
 
+    @keyword
     def set_state_active_low(self, dio_alias, active_low, ensure=True):
         """Set the power supply voltage goal
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].state.active_low.set(bool(active_low), ensure)
-
+        
     @keyword
     def set_state_polling_cycle(self, dio_alias:str, cycle:float, ensure=True):
         """Set the power supply voltage goal
