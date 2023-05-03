7f3570e723cb7fe97671203d006aff8b0ebe17ac
Paul FISHER <fisherhpaul@gmail.com>
2023-05-03 09:33:50 +0200

    control io by the gpio_id tree field

--- platform/panduza_platform/drivers/dio/pza_modbus_dio.py
+++ platform/panduza_platform/drivers/dio/pza_modbus_dio.py
@@ -8,7 +8,7 @@ from ...connectors.modbus_client_serial import ConnectorModbusClientSerial
 DIO_USBID_MODEL="05e1"
 DIO_USBID_VENDOR="16c0"
 DIO_USB_BAUDRATE=112500
-
+DIO_MODBUS_ADDR=1
 class DriverPZA_MODBUS_DIO(MetaDriverDio):
 
     def _PZADRV_config(self):
@@ -27,13 +27,16 @@ class DriverPZA_MODBUS_DIO(MetaDriverDio):
         """
         # settings = tree["settings"]
         # Load settings
-        settings = dict() if "settings" not in tree else tree["settings"]
-        settings["vendor"] = DIO_USBID_VENDOR
-        settings["model"] = DIO_USBID_MODEL
-        settings["baudrate"] = DIO_USB_BAUDRATE
-
-        self.modbus_connector = ConnectorModbusClientSerial.GetV2(**settings) # init the connector
+        self.settings = dict() if "settings" not in tree else tree["settings"]
+        self.settings["vendor"] = DIO_USBID_VENDOR
+        self.settings["model"] = DIO_USBID_MODEL
+        self.settings["baudrate"] = DIO_USB_BAUDRATE
+        self.settings["usb_serial_id"]
+        DIO_GPIO_ID = self.settings["gpio_id"]
         
+        self.modbus = ConnectorModbusClientSerial.GetV2(**self.settings) # init the connector
+        self.log.info(tree["settings"])
+
         # first configuration
         self.__dir = {
             "direction":{
@@ -43,52 +46,63 @@ class DriverPZA_MODBUS_DIO(MetaDriverDio):
             },
             "state":{
                 "active":True,
-                "active_low":True,
+                "active_low":False,
                 "polling_cycle":5
             }    
         }
-
+        
         super()._PZADRV_loop_init(tree) # update tree
-
+    
     def _PZADRV_loop_run(self):
         pass
     def _PZADRV_loop_err(self):
         pass
-
+    
     def _PZADRV_DIO_get_direction_value(self):
         self.log.info(f"read direction value : {self.__dir['direction']['value']} !")
-
         return self.__dir["direction"]["value"]
-
-    def _PZADRV_DIO_set_direction_value(self, v):
+    
+    # configure the direction and value of io 
+    def _PZADRV_DIO_set_direction_value(self, v): # vlaue direction (in/out)
         self.log.info(f"set direction value : {v}")
         self.__dir["direction"]["value"] = v
+        io = self.settings["gpio_id"] # get the gpio_id number
+        self.log.debug(f"value of io repeated {io}")
 
-        if self.__dir['direction']['value'] == v:
-            self.log.info(f"turn on led {v}")
-            self.modbus_connector.write_coil(v,True,1)
-            self.modbus_connector.write_coil(v+1,False,1)
-            self.modbus_connector.write_coil(64+v,True,1)
+        if v == "out":    
+            self.log.info(f"it's a output")
+            self.modbus.write_coil(int(io),True,DIO_MODBUS_ADDR) # configure output+
+            self.modbus.write_coil(64+int(io),1,DIO_MODBUS_ADDR)  # write to coil
+            
+            test = self.modbus.read_discrete_inputs(int(io)+1,1,DIO_MODBUS_ADDR)
+            self.log.warning(f"value of IO 1 {test}")
             time.sleep(2)
-            self.modbus_connector.write_coil(64+v,False,1)
+            self.modbus.write_coil(64+int(io),False,DIO_MODBUS_ADDR)
 
-    def _PZADRV_DIO_get_direction_pull(self):
-        self.log.info(f"read direction pull : {self.__dir['direction']['pull']}!")
-        return self.__dir["direction"]["pull"]
+        #         
+        # if self.__dir["direction"]["value"] == "out":
+        #     self.log.info(f"turn on io {DIO_USB_GPIO_CONTROL}")
+        #     self.modbus.write_coil(DIO_USB_GPIO_CONTROL,True,DIO_MODBUS_ADDR) # configure output
+        #     self.modbus.write_coil(64+DIO_USB_GPIO_CONTROL,True,DIO_MODBUS_ADDR)  # write to coil
+        #     time.sleep(2)
+        #     self.modbus.write_coil(64+DIO_USB_GPIO_CONTROL,False,DIO_MODBUS_ADDR)
 
     def _PZADRV_DIO_set_direction_pull(self, v):
         self.log.info(f"set direction pull : {v}")
         self.__dir["direction"]["pull"] = v
-        gpio = self.__dir["direction"]["value"]
+        # gpio = self.__dir["direction"]["value"]
 
-        if self.__dir["direction"]["pull"]  == "up":
-            self.log.info("set gpio as pull up")
-            self.modbus_connector.write_coil(gpio+32, True,1)
-        elif self.__dir["direction"]["pull"]  == "down":
-            self.log.info("set gpio as pull down")
-            self.modbus_connector.write_coil(gpio+32, False,1)
+        # if self.__dir["direction"]["pull"]  == "up":
+        #     self.log.info("set gpio as pull up")
+        #     self.modbus.write_coil(DIO_USB_GPIO_CONTROL+32, True,DIO_MODBUS_ADDR)
+        # elif self.__dir["direction"]["pull"]  == "down":
+        #     self.log.info("set gpio as pull down")
+        #     self.modbus.write_coil(DIO_USB_GPIO_CONTROL+32, False,DIO_MODBUS_ADDR)
 
-
+    def _PZADRV_DIO_get_direction_pull(self):
+        self.log.info(f"read direction pull : {self.__dir['direction']['pull']}!")
+        return self.__dir["direction"]["pull"]
+        
     def _PZADRV_DIO_get_state_active(self):
         self.log.info(f"read state active : {self.__dir['state']['active']}!")
         return self.__dir["state"]["active"]
