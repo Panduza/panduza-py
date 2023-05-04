20d4c4a2d7f9f2152745c3612bf813c54cb33b76
Paul FISHER <fisherhpaul@gmail.com>
2023-05-04 13:50:22 +0200

    add dio script for robot framework tests

--- client/panduza/robof/dio.py
+++ client/panduza/robof/dio.py
@@ -10,21 +10,21 @@ class KeywordsDio(object):
 
     @keyword
     def set_direction_value(self, dio_alias, direction, ensure=True):
-        """Set the power supply voltage goal
+        """Set the direction value
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].direction.value.set(float(direction), ensure)
     
     @keyword
     def set_direction_pull(self, dio_alias, pull, ensure=True):
-        """Set the power supply voltage goal
+        """Set the direction pull
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].direction.pull.set(float(pull), ensure)
 
     @keyword
     def set_direction_polling_cycle(self, dio_alias:str, cycle:float, ensure=True):
-        """Set the power supply voltage goal
+        """Set the direction polling cycle
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].direction.polling_cycle.set(float(cycle), ensure)
@@ -36,21 +36,21 @@ class KeywordsDio(object):
 
     @keyword
     def set_state_active(self, dio_alias, active, ensure=True):
-        """Set the power supply voltage goal
+        """Set the state active
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].state.active.set(bool(active), ensure)
 
     @keyword
     def set_state_active_low(self, dio_alias, active_low, ensure=True):
-        """Set the power supply voltage goal
+        """Set the state active low
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].state.active_low.set(bool(active_low), ensure)
         
     @keyword
     def set_state_polling_cycle(self, dio_alias:str, cycle:float, ensure=True):
-        """Set the power supply voltage goal
+        """Set the state polling cycle
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
         pza[dio_alias].state.polling_cycle.set(float(cycle), ensure)
