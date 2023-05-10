30707237b9465498ed416da3c87d990a6b2125a6
Paul FISHER <fisherhpaul@gmail.com>
2023-05-10 12:11:11 +0200

    remove unused code

--- platform/panduza_platform/drivers/dio/pza_modbus_dio.py
+++ platform/panduza_platform/drivers/dio/pza_modbus_dio.py
@@ -70,7 +70,6 @@ class DriverPZA_MODBUS_DIO(MetaDriverDio):
 
     def _PZADRV_DIO_get_direction_value(self):
         self.log.info(f"read direction value : {self.__dir['direction']['value']} !")
-        gpio_id = self.settings["gpio_id"]
         return self.__dir["direction"]["value"]
 
 
