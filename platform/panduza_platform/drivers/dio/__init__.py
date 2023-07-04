30dc1d365782a50629b7f072857b36da39edb28a
UTHAYACHELVAN <rethusan2000@gmail.com>
2023-07-04 16:10:19 +0200

    remove  boundary scan's driver

--- platform/panduza_platform/drivers/dio/__init__.py
+++ platform/panduza_platform/drivers/dio/__init__.py
@@ -1,7 +1,6 @@
 
 from .drv_panduza_fake_dio import DriverFakeDio
-from .drv_ftdi_boundary_scan_dio import DriverBoundaryScanDio
 
 
-PZA_DRIVERS_LIST= [DriverFakeDio,DriverBoundaryScanDio]
+PZA_DRIVERS_LIST= [DriverFakeDio]
 
