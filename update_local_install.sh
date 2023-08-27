f8ce3ec066fedf37ef12928fdba312ece1f9313c
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2023-08-27 08:01:42 +0200

    append update_local_install.sh to help devs

---
+++ update_local_install.sh
@@ -0,0 +1,10 @@
+
+# ===================================================================
+# PARAMETERS
+PYTHON_VENV_PATH=/usr/local/bin/panduza/venv
+
+# ===================================================================
+
+# Just update the current installation with the client and platform from this source
+${PYTHON_VENV_PATH}/bin/pip3 install client/
+${PYTHON_VENV_PATH}/bin/pip3 install platform/
