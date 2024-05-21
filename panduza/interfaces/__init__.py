6c7e2df20c174a7d9889251bddca12051e941c64
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-05-21 07:41:40 +0200

    append the ping interface support (#95)

--- panduza/interfaces/__init__.py
+++ panduza/interfaces/__init__.py
@@ -8,6 +8,8 @@ from .blc import Blc
 from .powermeter import Powermeter
 from .thermometer import Thermometer
 
+from .ping import Ping
+
 # from .serial_ import Serial
 # from .modbus_client import ModbusClient
 # from .ftdi_spi_client import Ftdi_Spi
