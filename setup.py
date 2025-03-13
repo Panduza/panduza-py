3c22f4f67d9fe6685d265150ee531d07398433a0
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-03-13 21:08:28 +0100

    up

--- setup.py
+++ setup.py
@@ -14,13 +14,9 @@ setup(
         install_requires=[
             'setuptools',
             'paho-mqtt==2.1.0',
+            'flatbuffers==25.2.10'
         ],
 
-        # entry_points={
-        #     'console_scripts': [
-        #         'pzadmin=panduza.admin.pzadmin:pzadmin_main',
-        #     ],
-        # },
 
         classifiers= [
             "Development Status :: 3 - Alpha",
